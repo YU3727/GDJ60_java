a82434d11109ef4e4ea6767696ea3a0a1f80582d
YU3727 <lacvert3727@naver.com>
2023-01-03 14:45:14 +0900

    test

--- src/com/pooh/s4/objects2/Main.java
+++ src/com/pooh/s4/objects2/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		//2-1.Main에서 ReturnTest의 t1, t2를 호출해보자
 		ReturnTest returnTest = new ReturnTest();
 		
-		returnTest.t1(); //return값이 void라는 것은 호출만 해라는 의미
+		returnTest.t1(); //return값이 void라는 것은 호출만 하라는 의미
 		
 		returnTest.t2(); //이 명령의 의미는 (홀>주방에)짜장면주세요~ 라는 요청같은 의미. 주방에서 짜장면을 요리해서 내놨는데 홀에서 가져가지 않는 형태.
 		
@@ -18,6 +18,7 @@ public class Main {
 		for(int i=0; i<3; i++) {
 			System.out.println(ar[i]);
 		}
+		//git-Hub 업로드 확인용 주석 추가
 	}
 
 }
